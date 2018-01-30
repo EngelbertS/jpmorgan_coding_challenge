a626c3bff1307aa03d0548e3e85a74e77f5cadb8
unknown <nero100@gmx.net>
2018-01-30 11:54:20 +0100

    Main added

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.text.ParseException;
+
+public class Main {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+		
+       Trading trading = new Trading();
+       trading.execute();	
+	}
+
+}
